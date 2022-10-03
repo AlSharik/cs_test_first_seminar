6682805191c406c7d18f5d1bf158fb485a44c107
Alexander Shar <alahan05@gmail.com>
2022-10-03 16:18:16 +0300

    4th task end

---
+++ seminar1task4/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Write any number:");
+int max = Convert.ToInt32(Console.ReadLine());
+int min = max * -1;
+
+if (max <= min)
+{
+    max = max * -1;
+    min = min * -1;
+}
+
+while(min <= max)
+{
+    System.Console.WriteLine(min);
+    min++;
+}
