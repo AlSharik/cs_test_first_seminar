fbcc70bd96c5897ee3c4115bae8382b4ed71d5b6
Alexander Shar <alahan05@gmail.com>
2022-10-03 13:59:38 +0300

    Another solution of second task with int.Parse

--- seminar1task3/Program.cs
+++ seminar1task3/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Prompt( )
+{
+    Console.WriteLine("Write number >");
+    string inputString = Console.ReadLine();
+    int value = Convert.ToInt32(inputString);
+    return value;
+}
+
+int day = Prompt();
+switch(day)
+{
+    case 1:
+        System.Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        System.Console.WriteLine("Вторник");
+        break;
+    case 3:
+        System.Console.WriteLine("Среда");
+        break;
+    case 4:
+        System.Console.WriteLine("Четверг");
+        break;
+    case 5:
+        System.Console.WriteLine("Пятницы");
+        break;
+    case 6:
+        System.Console.WriteLine("Суббота");
+        break;
+    case 7:
+        System.Console.WriteLine("Воскресенье");
+        break;
+    default:
+        System.Console.WriteLine("Такого дня недели не существует");
+        break;
+}
