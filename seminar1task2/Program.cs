bf2c119fbf1ddb948a787836b5a9e0adea1aeac2
Alexander Shar <alahan05@gmail.com>
2022-10-03 14:00:14 +0300

    Another solution of second task with int.Parse

--- seminar1task2/Program.cs
+++ seminar1task2/Program.cs
@@ -1,4 +1,6 @@
-﻿int Prompt( )
+﻿/*
+Первый вариант, до того как я узнал у существовании int.parse
+int Prompt( )
 {
     Console.WriteLine("Write number >");
     string inputString = Console.ReadLine();
@@ -8,6 +10,22 @@
 
 int number = Prompt();
 int square = Prompt();
+if(number * number == square)
+{
+    System.Console.WriteLine("Yes");
+}
+else
+{
+    System.Console.WriteLine("No");
+}
+*/
+
+System.Console.WriteLine("Write number");
+int number = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Write square");
+int square = int.Parse(Console.ReadLine());
+
 if(number * number == square)
 {
     System.Console.WriteLine("Yes");
